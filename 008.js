1fef57e9c7950fb37dbbe19e45d1e464f42ceb6d
Chris Grant <cgrant@scottlogic.co.uk>
2015-09-17 10:46:19 +0100

    Finished "HTTP Collect"

---
+++ 008.js
@@ -0,0 +1,18 @@
+var bl = require('bl')
+var http = require('http');
+
+var url = process.argv[2];
+
+http.get(url, function(response) {
+
+	response.pipe(bl(function (err, data) {
+		if (err) {
+      return console.error(err)
+		}
+
+		var dataString = data.toString();
+		console.log(dataString.length)
+		console.log(dataString);
+	}));
+
+});
